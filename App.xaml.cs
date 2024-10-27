813ef91c419907b45925e839159e00fe24fc9567
ManNg <ng.m1911g@gmail.com>
2024-10-27 19:01:44 +0700

    Source init

---
+++ App.xaml.cs
@@ -0,0 +1,48 @@
+﻿using AutoGenerateContent.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace AutoGenerateContent
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static IHost AppHost { get; private set; } = default!;
+
+        public App()
+        {
+            AppHost = Host.CreateDefaultBuilder()
+                .ConfigureServices((HostBuilderContext, services) =>
+                {
+                    services.AddDbContext<SQLiteContext>(options => options.UseSqlite("Data Source=SQLiteDb.db"));
+
+                    Log.Logger = new LoggerConfiguration()
+                            .WriteTo.File("Logs/.txt", rollingInterval: RollingInterval.Day)
+                            .CreateLogger();
+                })
+                .Build();
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await AppHost!.StartAsync();
+
+            using (var scope = AppHost.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<SQLiteContext>();
+                db.Database.EnsureCreated();
+                db.Database.Migrate();
+            }
+
+            base.OnStartup(e);
+        }
+    }
+
+}
---
+++ DatabaseContext/SQLiteContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Serilog;
+
+namespace AutoGenerateContent.DatabaseContext
+{
+    public class SQLiteContext(DbContextOptions<SQLiteContext> options) : DbContext(options)
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                          .LogTo(x => Log.Logger.Debug(x),
+                                    events:
+                                    [
+                                        RelationalEventId.CommandExecuted,
+                                    ])
+                          .EnableDetailedErrors()
+                          .EnableSensitiveDataLogging();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Global filter here
+            //modelBuilder.Entity<Table>().HasQueryFilter(b => b.IsDeleted = "1");
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
