5347a766496adc8a8b67184a3b50650f061af18b
danhkunz <danh.d.luu@tgl-sol.com>
2024-11-01 15:29:52 +0700

    Update binding data

---
+++ Models/ConfigViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoGenerateContent.Models
+{
+    [NotMapped]
+    public class ConfigViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
